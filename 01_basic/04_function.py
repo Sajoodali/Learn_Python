052da318a800412e2c566b5f684655e3ccdc0531
sajood ali <sajoodali486@gmail.com>
2024-12-01 11:46:50 +0500

    creat fumction

---
+++ 01_basic/04_function.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_otp(length=5):
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+    return otp
+
+otp = generate_otp()
+print(f"Your OTP is: {otp}")
